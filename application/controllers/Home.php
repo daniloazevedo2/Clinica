6369f089098ba232b537224b22270e7989173503
Danilo Azevedo <azevedo.danilo21@gmail.com>
2018-01-23 09:24:40 -0400

    includes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,9 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('includes/html_header');
+		$this->load->view('includes/menu');
 		$this->load->view('view_home');
+		$this->load->view('includes/html_footer');
 	}
 }
---
+++ application/views/includes/html_header.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/med.png">
+
+    <title>Clínica</title>
+
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/css/dashboard.css" rel="stylesheet">
+  </head>
+
+  <body>
