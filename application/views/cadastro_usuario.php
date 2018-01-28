7ede39faec7e4a77fc968e26bfa914e59ef66266
Danilo Azevedo <azevedo.danilo21@gmail.com>
2018-01-27 21:00:44 -0400

    cadastro

--- application/views/cadastro_usuario.php
+++ application/views/cadastro_usuario.php
@@ -1,8 +1,65 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-    <h1 class="h2">Novo Usuário</h1>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+        <h3>Novo Usuário</h1>
+    </div>
+    <div class="col-md-12">
+        <form>
+            <div class="form-group">
+                <label for="nome">Nome: </label>
+                <input type="nome" class="form-control" name="nome" id="nome" placeholder="Informe o nome..." required>
+            </div>
 
-       
-  </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label for="cpf">CPF: </label>
+                        <input type="cpf" class="form-control" name="cpf" id="cpf" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" onkeyup="somenteNumeros(this);" placeholder="Informe o CPF..." required>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="form-group">
+                        <label for="endereco">Endereço: </label>
+                        <input type="endereco" class="form-control" name="endereco" id="endereco" placeholder="Informe o endereço..." required>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <label for="nivel">Nível: </label>
+                    <select id="nivel" class="form-control">
+                        <option value="0"> --- </option>
+                        <option value="1"> Administrador </option>
+                        <option value="2"> Usuário </option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="email">Email: </label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Informe o Email..." required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="senha">Senha: </label>
+                        <input type="password" class="form-control" name="senha" id="senha" placeholder="Informe a senha..." required>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <label for="status">Status: </label>
+                    <select id="status" class="form-control">
+                        <option value="0"> ---</option>
+                        <option value="1"> Ativo</option>
+                        <option value="2"> Inativo</option>
+                    </select>
+                </div>
+            </div>
+
+            <div style="text-align: right">
+                <button type="submit" class="btn btn-success">Enviar</button>
+                <button type="reset" class="btn btn-danger">Cancelar</button>
+            </div>
+        </form>
+    </div>
 
 
--- application/views/includes/html_header.php
+++ application/views/includes/html_header.php
@@ -1,19 +1,40 @@
 
 <!doctype html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="assets/img/med.png">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="assets/img/med.png">
 
-    <title>Clínica</title>
+        <title>Clínica</title>
 
-    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url(); ?>assets/css/dashboard.css" rel="stylesheet">
-    <link href="<?= base_url(); ?>assets/css/sub.css" rel="stylesheet">
+        <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?= base_url(); ?>assets/css/dashboard.css" rel="stylesheet">
+        <link href="<?= base_url(); ?>assets/css/sub.css" rel="stylesheet">
 
-  </head>
+    </head>
 
-  <body>
+    <body>
+        <script>
+            function formatar(mascara, documento) {
+                var i = documento.value.length;
+                var saida = mascara.substring(0, 1);
+                var texto = mascara.substring(i)
+
+                if (texto.substring(0, 1) != saida) {
+                    documento.value += texto.substring(0, 1);
+
+                }
+
+            }
+            function somenteNumeros(num) {
+                var er = /[^0-9.-]/;
+                er.lastIndex = 0;
+                var campo = num;
+                if (er.test(campo.value)) {
+                    campo.value = "";
+                }
+            }
+        </script>
